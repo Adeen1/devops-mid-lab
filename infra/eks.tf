509accc9cfa7395d21b91573dc8d2cbf21045efc
Muhammad Ahmad <95732674+Qdevaan@users.noreply.github.com>
2025-12-17 18:49:22 +0500

    Fix Infra: Update EKS to 1.30 and use public subnets (avoid EIP limit)

--- infra/eks.tf
+++ infra/eks.tf
@@ -3,10 +3,10 @@ module "eks" {
   version = "19.20.0"
 
   cluster_name    = "lab-exam-cluster"
-  cluster_version = "1.27"
+  cluster_version = "1.30"
 
   vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  subnet_ids                     = module.vpc.public_subnets # Use public subnets to avoid NAT GW requirement
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
